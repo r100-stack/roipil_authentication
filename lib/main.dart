a9ab95c17168f4b2645b216a1e24055baa20b66b
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-12-28 20:50:03 -0600

    Removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
