2d99144038f251e57c34d9b62a844ef27716cc1b
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2021-01-03 21:23:07 -0600

    Removed RoipilAuth picture. Added a TODO in auth_bloc

--- lib/blocs/auth_bloc.dart
+++ lib/blocs/auth_bloc.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
 
 class AuthBloc { // TODO: Does it need to extend ChangeNotifier (Provider implementation) or is just static enough
+// TODO: May need to remove the onAuthChanged from auth_bloc to avoid confusion with auth_bloc from other projects.
   static Function(auth.User) _onAuthChanged;
 
   static Function(auth.User) get onAuthChanged => _onAuthChanged;
